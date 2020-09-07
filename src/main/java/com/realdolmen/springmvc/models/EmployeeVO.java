eefc98e2954a8b5dc810c5ecb1c5a6e248ca84ad
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-09-07 11:05:14 +0200

    started tutorial

---
+++ src/main/java/com/realdolmen/springmvc/models/EmployeeVO.java
@@ -0,0 +1,21 @@
+package com.realdolmen.springmvc.models;
+
+import java.io.Serializable;
+
+package com.realdolmen.springmvc.models;
+
+public class EmployeeVO {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    //Setters and Getters
+
+    @Override
+    public String toString() {
+        return "EmployeeVO [id=" + id + ", firstName=" + firstName
+                + ", lastName=" + lastName + "]";
+    }
+}
---
+++ src/main/java/com/realdolmen/springmvc/services/EmployeeManager.java
@@ -0,0 +1,4 @@
+package com.realdolmen.springmvc.services;
+
+public class EmployeeManager {
+}
